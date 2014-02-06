d4d0e977b46f2f647ffb41f3816467deb5d0b71c
ElenaHoshizaki <elena.hoshizaki@gmail.com>
2014-02-06 12:32:35 -0800

    add app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WDIwarts::Application.config.secret_key_base = '5211406bf069726d43e84a98b6093c6f5766ebc47efb29931683bcdf8b676152eb4b85fb25d8ceccc229e1182a53549563bf87e8c3574abd40e6538db1494061'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WDIwarts::Application.config.session_store :cookie_store, key: '_WDIwarts_session'
